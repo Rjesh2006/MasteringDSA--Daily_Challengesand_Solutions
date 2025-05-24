0524a3db2dea9c4a24cec3212803f1615123fb75
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2025-05-24 23:57:34 +0530

    Create commnds.tf

---
+++ commnds.tf
@@ -0,0 +1,80 @@
+rm validate
+ 1993  terraform plan
+ 1994  terraform state list
+ 1995  terraform ls show
+ 1996  terraform list shoe
+ 1997  terraform list show
+ 1998  terrafrom show liset
+ 1999  terrafrom show list
+ 2000  cl
+ 2001  sudo apt install unzip
+ 2002  unzip awscliv2.zip 
+ 2003  cl
+ 2004  terraform apply
+ 2005  terraform validate
+ 2006  terreaaform init
+ 2007  terraform init
+ 2008  terraform apply
+ 2009  terraform init
+ 2010  terraform apply
+ 2011  terraform workspace crete new dev
+ 2012  terraform workspace new dev
+ 2013  terraform state list
+ 2014  terraform wokspace select default
+ 2015  yeah 
+ 2016  terraform workspace select default
+ 2017  terraform state list
+ 2018  cl
+ 2019  git status
+ 2020  terraform workspace switch dev
+ 2021  terraform workspace select dev
+ 2022  git init
+ 2023  git status
+ 2024  git checkout dev
+ 2025  git checkout -b dev
+ 2026  git branch 
+ 2027  git add.
+ 2028  git add .
+ 2029  git commit -m "terraform added"
+ 2030  git config --global user.name "Rjesh2006"  # or "Rajesh Kumar" if you prefer
+ 2031  git config --global user.email "rk5267124@gmail.com"
+ 2032  git add .
+ 2033  git commit -m "terraform added"
+ 2034  git status
+ 2035  terraform workspace list
+ 2036  git add . 
+ 2037  git commit -m "environment added" 
+ 2038  git checkout -b master
+ 2039  terraform select default
+ 2040  terraform workspace select default
+ 2041  git status
+ 2042  terraform apply
+ 2043  git status
+ 2044  git push -u origin master
+ 2045  terraform workspace list
+ 2046  git status
+ 2047  git add .
+ 2048  git commmit -m "added prodcutiion code"
+ 2049  git commit -m "production code added" 
+ 2050  terraform workspace select dev
+ 2051  git sttus
+ 2052  git status
+ 2053  terraform workspace list
+ 2054  terraform apply
+ 2055  git status
+ 2056  terraform workspace list
+ 2057  git add .
+ 2058  git checkout master
+ 2059  git checkout dev
+ 2060  git commit -m "commit added"
+ 2061  git checkout dev
+ 2062  git checkout master
+ 2063  git add .
+ 2064  git commit -m "comted"
+ 2065  git checkout master
+ 2066  terraform status
+ 2067  terraform 
+ 2068  terraform status
+ 2069  terraform workspace list
+ 2070  sudo history
+ 2071  history
